4b20dfc4d6648fd88a72fb0dcb59ff303ba4082a
GRONDIN Maeva <maegrondin@ecstazy.pedago.ipb.fr>
2018-01-22 11:39:17 +0100

    hgt lat et long

--- projet-map-reduce/src/main/java/Main.java
+++ projet-map-reduce/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,27 +12,37 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.input.PortableDataStream;
+
+import scala.reflect.internal.Trees.Return;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		SparkConf conf = new SparkConf().setAppName("TP Spark");
-		JavaSparkContext context = new JavaSparkContext(conf);
-		
-		JavaRDD<Integer> rdd;
-		rdd = context.parallelize(Arrays.asList(1, 2, 8, 7, 2),10);
+
 		
 
-		JavaPairRDD<String, String> rdd2 = context.wholeTextFiles("/home/tagry/Documents/Ecole/ProjetBigData/BigDataProject/FilesHgt");
-		JavaRDD<int[][]> rddKey = rdd2.keys().map((path)-> hgtConvert(path));
-		List<int[][]> myList = rddKey.collect();
+		SparkConf conf = new SparkConf().setAppName("TP Spark");
+		JavaSparkContext context = new JavaSparkContext(conf);
+
+		// JavaRDD<Integer> rdd;
+		// rdd = context.parallelize(Arrays.asList(1, 2, 8, 7, 2),10);
+
 		
-		System.out.println("bleblebkjedkjhlkjhlkjhkljhlkjhkljhh" + myList);
+
+		JavaPairRDD<String, PortableDataStream> rdd2 = context
+				.binaryFiles("/dem3_raw/N00E033.hgt");
 		
-		
-	}
+		JavaRDD<HgtData> rddKey = rdd2.map((tuple) -> hgtConvertToClass(
+				tuple._1, tuple._2));
 	
+		
+		JavaRDD<String> rddToString = rddKey.map(data -> data.toString());
+		rddToString.saveAsTextFile("/user/maegrondin/aaaaaaaaaaaaaaaaaaaaaaa0000000000000000");
+		
+
+	}
+
 	public static int[][] hgtConvert(String filePath) {
 		filePath = filePath.substring(5);
 		File file = new File(filePath);
@@ -55,8 +66,40 @@ public class Main {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-		
+		}
+
 		return height;
+	}
+
+	public static HgtData hgtConvertToClass(String filePath,
+			PortableDataStream file) {
+		DataInputStream data = file.open();
+		
+		System.out.println(">>>>>>>>>>>>>>>>> FILE PATH : " + filePath);
+
+		int latitude = Integer.parseInt(filePath.substring(30, 32));
+		int longitude = Integer.parseInt(filePath.substring(33, 36));
+
+		int[][] height = new int[1201][1201];
+		try {
+			char[] buf = new char[2];
+
+			for (int i = 0; i < 1201; i++) {
+				for (int j = 0; j < 1201; j++) {
+					buf[0] = data.readChar();
+					buf[1] = data.readChar();
+					height[i][j] = (buf[0] << 8) | buf[1];
+				}
+				data.close();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new HgtData(latitude, longitude, height);
 	}
 }
